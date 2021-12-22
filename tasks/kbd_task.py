bb5b6fb2afd8511d752dc1c39b6e6cd52ace4b7e
Bastien Orivel <bastien@technocreatives.com>
2021-12-22 11:04:34 +0100

    And a macos build for keyboards too

--- tasks/kbd_task.py
+++ tasks/kbd_task.py
@@ -11,14 +11,15 @@ def create_kbd_task(os):
             .with_gha("init", GithubAction("Eijebong/divvun-actions/pahkat/init", {"repo": "https://pahkat.uit.no/devtools/", "channel": "nightly", "packages": "pahkat-uploader, kbdgen" }))
             .with_gha("build", GithubAction("Eijebong/divvun-actions/keyboard/build", {"keyboard-type": "keyboard-windows"}))
             .with_prep_gha_tasks()
-            .find_or_create("bundle.%s_x64.%s" % (os, CONFIG.git_sha))
+            .find_or_create("kbdgen.%s_x64.%s" % (os, CONFIG.git_sha))
         )
     elif os == "macos-latest":
-        return
         return (
-            macos_task("Bundle lang: %s %s" % (os, type_))
-            .with_curl_artifact_script(lang_task_id, "spellcheckers.bundle.tar.gz")
-            .find_or_create("bundle.%s_x64_%s.%s" % (os, type_, CONFIG.git_sha))
+            macos_task("Build keyboard: %s" % (os))
+            .with_gha("setup", GithubAction("Eijebong/divvun-actions/setup", {}).with_secret_input("key", "divvun", "DIVVUN_KEY"))
+            .with_gha("init", GithubAction("Eijebong/divvun-actions/pahkat/init", {"repo": "https://pahkat.uit.no/devtools/", "channel": "nightly", "packages": "pahkat-uploader, kbdgen, xcnotary" }))
+            .with_gha("build", GithubAction("Eijebong/divvun-actions/keyboard/build", {"keyboard-type": "keyboard-macos"}))
+            .find_or_create("kbdgen.%s_x64_%s.%s" % (os, CONFIG.git_sha))
         )
     else:
         raise NotImplemented
