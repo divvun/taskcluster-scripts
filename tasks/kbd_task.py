8a60a097d1a61aefb619dc36dcf6900df2eaa1ff
Bastien Orivel <bastien@technocreatives.com>
2022-01-05 13:36:00 +0100

    Install a nightly version of kbdgen on macos to try if it helps debugging

--- tasks/kbd_task.py
+++ tasks/kbd_task.py
@@ -17,7 +17,7 @@ def create_kbd_task(os):
         return (
             macos_task("Build keyboard: %s" % (os))
             .with_gha("setup", GithubAction("Eijebong/divvun-actions/setup", {}).with_secret_input("key", "divvun", "DIVVUN_KEY"))
-            .with_gha("init", GithubAction("Eijebong/divvun-actions/pahkat/init", {"repo": "https://pahkat.uit.no/devtools/", "channel": "nightly", "packages": "pahkat-uploader, kbdgen, xcnotary" }))
+            .with_gha("init", GithubAction("Eijebong/divvun-actions/pahkat/init", {"repo": "https://pahkat.uit.no/devtools/", "channel": "nightly", "packages": "pahkat-uploader, kbdgen@2.0.0-nightly.20210622T210632Z, xcnotary" }))
             .with_gha("build", GithubAction("Eijebong/divvun-actions/keyboard/build", {"keyboard-type": "keyboard-macos"}))
             .with_prep_gha_tasks()
             .find_or_create("kbdgen.%s_x64.%s" % (os, CONFIG.git_sha))
