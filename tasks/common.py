846b1514990bf0747924a8c1882700e679d2794c
Bastien Orivel <bastien@technocreatives.com>
2022-02-02 12:46:12 +0100

    Update ubuntu to 21.04 because their policy is to break unsupported things
    Apparently you're not allowed to keep using unsupported ubuntu versions,
    they'll just put the mirrors for that on the crappiest servers they can
    find.

--- tasks/common.py
+++ tasks/common.py
@@ -14,7 +14,7 @@ def linux_build_task(name, bundle_dest="repo", with_secrets=True):
         decisionlib.DockerWorkerTask(name)
         .with_worker_type("linux")
         .with_provisioner_id("divvun")
-        .with_docker_image("ubuntu:hirsute")
+        .with_docker_image("ubuntu:21.10")
         # https://docs.taskcluster.net/docs/reference/workers/docker-worker/docs/caches
         .with_scopes("docker-worker:cache:divvun-*")
         .with_scopes("queue:get-artifact:private/*")
