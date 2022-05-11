9c6c99f1789b89d8f73f310537cf14f6b515280b
Bastien Orivel <bastien@technocreatives.com>
2022-05-11 15:40:17 +0200

    Try fixing the MSO_VER

--- tasks/mso_resources.py
+++ tasks/mso_resources.py
@@ -21,11 +21,12 @@ def create_patch_gen_task():
         .with_gha("build_rust", GithubAction("actions-rs/cargo", {"command": "build"}).with_env("SENTRY_DSN", "${{ secrets.divvun.MSO_MACOS_DSN }}"))
         .with_gha("download_office", GithubActionScript(r"""
           for MSO_URL in $(node mso-patcher/dist/unpatched.js); do
-              export MSO_VER=$(echo $MSO_URL | sed -e 's/https:\/\/officecdn-microsoft-com.akamaized.net\/pr\/C1297A47-86C4-4C1F-97FA-950631F94777\/MacAutoupdate\/Microsoft_Word_\(.*\)_Installer\.pkg/\1/')
+              export MSO_VER=$(echo $MSO_URL | sed -e 's/https:\/\/officecdn-microsoft-com.akamaized.net\/pr\/C1297A47-86C4-4C1F-97FA-950631F94777\/MacAutoupdate\/Microsoft_\(.*\)_Installer\.pkg/\1/')
               echo $MSO_URL
               wget -O mso.pkg "$MSO_URL"
               sudo installer -verbose -pkg mso.pkg -target /
               rm -f mso.pkg
+              ls /Applications
               sudo mv "/Applications/Microsoft Word.app" $MSO_VER
               echo $MSO_VER
               break
