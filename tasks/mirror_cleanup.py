dcf316ad0d617e33bd1c9c14d2e5fc56840de873
Bastien Orivel <bastien@technocreatives.com>
2022-07-26 15:36:43 +0200

    Fix typo

--- tasks/mirror_cleanup.py
+++ tasks/mirror_cleanup.py
@@ -28,7 +28,7 @@ def create_mirror_cleanup_task():
         )
         .with_script("ssh-keyscan github.com > ~/.ssh/known_hosts")
         .with_additional_repo(
-            "git@github.com:kivvun/pahkat.uit.no-index", "index"
+            "git@github.com:divvun/pahkat.uit.no-index", "index"
         )
         .with_script("cd repo && git pull origin main")
         .find_or_create(f"cleanup.pahkat.uit.no.{CONFIG.index_path}")
