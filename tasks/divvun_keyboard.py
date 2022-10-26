d0f8f6bd8cc8bb4e2e4ae2358f31846cf6e635c4
Bastien Orivel <bastien@technocreatives.com>
2022-10-26 12:43:38 +0200

    According to fastlane#20741 this should fix our publishing issue

--- tasks/divvun_keyboard.py
+++ tasks/divvun_keyboard.py
@@ -62,7 +62,8 @@ def create_ios_keyboard_task(bundle, is_dev):
                 % ipa_name
             )
             .with_env("SPACESHIP_SKIP_2FA_UPGRADE", 1)
-            .with_env("LANG", "en_US.UTF-8"),
+            .with_env("LANG", "en_US.UTF-8")
+            .with_env("ITMSTRANSPORTER_FORCE_ITMS_PACKAGE_UPLOAD", "true"),
             enabled=(CONFIG.git_ref == "refs/heads/main"),
         )
         .find_or_create(f"keyboard-build.ios.{CONFIG.index_path}")
