93fec9f57fbc63fb902d7896bb41f8d7d5350499
Dylan Hand <dylan.hand@gmail.com>
2024-02-29 12:23:52 +1000

    Going crazy

--- tasks/lang_task.py
+++ tasks/lang_task.py
@@ -226,28 +226,28 @@ def create_bundle_task(os_name, type_, lang_task_id):
                     },
                 ).with_outputs_from(lang_task_id),
             )
-            .with_gha(
-                "codesign",
-                GithubAction(
-                    "divvun/taskcluster-gha/codesign",
-                    { "path": "${{ steps.bundler.outputs['payload-path'] }}" },
-                ),
-            )
             # .with_gha(
-            #     "deploy",
+            #     "codesign",
             #     GithubAction(
-            #         "divvun/taskcluster-gha/speller/deploy",
-            #         {
-            #             "speller-type": type_,
-            #             "speller-manifest-path": "manifest.toml",
-            #             "payload-path": "${{ steps.codesign.outputs['payload-path'] }}",
-            #             "version": "${{ steps.version.outputs.version }}",
-            #             "channel": "${{ steps.version.outputs.channel }}",
-            #             "repo": "https://pahkat.uit.no/main/",
-            #             "nightly-channel": NIGHTLY_CHANNEL,
-            #         },
+            #         "divvun/taskcluster-gha/codesign",
+            #         { "path": "${{ steps.bundler.outputs['payload-path'] }}" },
             #     ),
             # )
+            .with_gha(
+                "deploy",
+                GithubAction(
+                    "divvun/taskcluster-gha/speller/deploy",
+                    {
+                        "speller-type": type_,
+                        "speller-manifest-path": "manifest.toml",
+                        "payload-path": "${{ steps.bundler.outputs['payload-path'] }}",
+                        "version": "${{ steps.version.outputs.version }}",
+                        "channel": "${{ steps.version.outputs.channel }}",
+                        "repo": "https://pahkat.uit.no/main/",
+                        "nightly-channel": NIGHTLY_CHANNEL,
+                    },
+                ),
+            )
             .find_or_create(f"bundle.{os_name}_x64_{type_}.{CONFIG.index_path}")
         )
 
