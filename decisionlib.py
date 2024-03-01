6b14efa833b92e9967f7cb8bc7ab88dd20d0be45
Dylan Hand <dylan.hand@gmail.com>
2024-03-01 13:03:39 +1000

    Attempt to fix allowing multiple branches for gha

--- decisionlib.py
+++ decisionlib.py
@@ -491,20 +491,22 @@ class Task:
         if not enabled:
             return self
 
-        if gha.git_fetch_url and gha.git_fetch_url not in self.action_paths:
-            target = f"{gha.repo_name}-{branch}" if branch else gha.repo_name
-            print(f"target: {target}")
-
-            self.with_additional_repo(
-                gha.git_fetch_url,
-                os.path.join(SHARED.task_root_for(
-                    self.platform()), target),
-                branch=branch
-            )
-
+        if gha.git_fetch_url:
             action_path = f"{gha.git_fetch_url}-{branch}" if branch else gha.git_fetch_url
-            print(f"action_path: {action_path}")
-            self.action_paths.add(action_path)
+
+            if action_path not in self.action_paths:
+                target = f"{gha.repo_name}-{branch}" if branch else gha.repo_name
+                print(f"target: {target}")
+
+                self.with_additional_repo(
+                    gha.git_fetch_url,
+                    os.path.join(SHARED.task_root_for(
+                        self.platform()), target),
+                    branch=branch
+                )
+
+                print(f"action_path: {action_path}")
+                self.action_paths.add(action_path)
 
         if not any(
             CONFIG.git_ref == "refs/heads/%s" % branch for branch in DEPLOY_BRANCHES
