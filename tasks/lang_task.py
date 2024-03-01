2b74a84f07b2f2ee139db7049061d8f64263eec5
Dylan Hand <dylan.hand@gmail.com>
2024-03-01 15:06:00 +1000

    Yes, here too

--- tasks/lang_task.py
+++ tasks/lang_task.py
@@ -207,8 +207,6 @@ def create_bundle_task(os_name, type_, lang_task_id):
                         "packages": "pahkat-uploader",
                     },
                 ),
-                # TODO: remove branch when done developing
-                branch="windows-codesign",
             )
             .with_gha("setup", gha_setup())
             .with_gha(
@@ -239,6 +237,8 @@ def create_bundle_task(os_name, type_, lang_task_id):
                 GithubAction(
                     "divvun/taskcluster-gha/codesign",
                     { "path": "${{ steps.bundler.outputs['payload-path'] }}" },
+                    # TODO: remove branch when done developing
+                    branch="windows-codesign",
                 ),
             )
             .with_gha(
